faa3c828b87ac5cdaa84955c1995dc3c94c6fad9
Coahle Aub <aubcoahle123@gmail.com>
2026-05-14 14:02:57 +0200

    added a timeout function

--- index.js
+++ index.js
@@ -63,4 +63,7 @@ function makesound(key) {
     function buttonAnimation(currentKey) {
       var activeButton = document.querySelector("." + currentKey);
       activeButton.classList.add("pressed");
+            setTimeout(function() {
+        activeButton.classList.remove("pressed");
+      }, 100);
     }
